0ca7067808c573e39ed354b575dfac418d74de5a
mutdmour <mutdmour@gmail.com>
2017-07-02 16:24:18 +0300

    *

--- project/sampleModel/Homomorphic_Envelope_with_Hilbert.py
+++ project/sampleModel/Homomorphic_Envelope_with_Hilbert.py
@@ -36,27 +36,6 @@
 ## Outputs:
 # homomorphic_envelope: The homomorphic envelope of the original
 # signal (not normalised).
-#
-# This code was developed by David Springer for comparison purposes in the
-# paper:
-# D. Springer et al., ?Logistic Regression-HSMM-based Heart Sound 
-# Segmentation,? IEEE Trans. Biomed. Eng., In Press, 2015.
-#
-## Copyright (C) 2016  David Springer
-# dave.springer@gmail.com
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY;without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from scipy.signal import butter, filtfilt, hilbert
 import numpy as np
